53f172612fbb7edc98dcdd555444a9fb79edd184
Alisa Sira <alisa.sira@gmail.com>
2024-03-15 15:41:15 +0200

    [DEMO] Remove sensitive data.

--- demo-telegram-bot/src/main/java/ua/alisasira/tgbot/TelegramBotApplication.java
+++ demo-telegram-bot/src/main/java/ua/alisasira/tgbot/TelegramBotApplication.java
@@ -3,10 +3,13 @@ package ua.alisasira.tgbot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.telegram.telegrambots.starter.TelegramBotStarterConfiguration;
 
 @SpringBootApplication
 @Import(TelegramBotStarterConfiguration.class)
+@PropertySources(value = @PropertySource("classpath:/bot.properties"))
 public class TelegramBotApplication {
     public static void main(String[] args){
         SpringApplication.run(TelegramBotApplication.class, args);
---
+++ demo-validation/README.md
@@ -0,0 +1,7 @@
+### Java practical test assignment for Clear Solutions
+
+# Materials used for implementation
+- Validator implementation: https://reflectoring.io/bean-validation-with-spring-boot/#using-validation-groups-to-validate-objects-differently-for-different-use-cases
+- Error handling: https://www.baeldung.com/exception-handling-for-rest-with-spring
+- Date parameter parsing: https://www.baeldung.com/spring-date-parameters
+- Jsoup Http Client: https://jsoup.org/
